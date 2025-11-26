f724b7a92ddef3ee7734ccffaffc075c60b74b45
abel592 <abeljosuecolquehuanca12@gmail.com>
2025-11-26 10:52:57 -0500

    cambios 2

---
+++ ms-pcge/src/main/java/com/example/ms_pcge/feign/UserFeignClient.java
@@ -0,0 +1,16 @@
+package com.example.ms_pcge.feign;
+
+
+import com.example.ms_pcge.dto.AuthUserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "ms-auth-service", path = "/auth")
+public interface UserFeignClient {
+
+
+    @GetMapping("/{id}")
+    AuthUserDto getUserById(@PathVariable Long id);
+}
